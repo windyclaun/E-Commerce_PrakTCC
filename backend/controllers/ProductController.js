d964d1f18302d23229dbc9953efc1cccf7d7f342
ERU16 <bagasduta4@gmail.com>
2025-05-27 22:59:04 +0700

    coba coba

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/ProductModel");
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
-const bucketName = process.env.GCP_BUCKET_NAME;
+const bucketName = "ecommerce-bucket-project";
 const bucket = storage.bucket(bucketName);
 const path = require("path");
 
